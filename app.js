3ecac0d74118c96c9928b979fdd6e65c29ef9187
Chadcramer <chadpcramer@gmail.com>
2019-04-22 12:31:29 -0500

    react components in progress

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-// TODO
+let GroceryList = (props) => {
+    
+    let onListItemClick = (event) => {
+          console.log('I got clicked');
+      };
+
+    return (
+      <ul>
+        <li onClick={onListItemClick}>{ props.list[0] }</li> 
+        <li>{ props.list[1] }</li> 
+        <li>{ props.list[2] }</li> 
+    </ul>
+    );
+}
+
+
+let App = () => (
+    <div>
+        <h1>My Grocery List</h1>
+        <GroceryList list= { ['hemp seeds', 'almonds', 'bananas'] }/>
+    </div>
+  );
+
+ReactDOM.render(<App />, document.getElementById("app"));
